ce6806360ce03c48f8ed56a3c3165d2e65d7e935
dinhdev-nu <truongbadinh13579@gmail.com>
2025-10-15 17:46:52 +0700

    (feat): Add toast feature for client notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Routes from "./Routes";
+import { Toaster } from "components/ui/Toaster";
 
 function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <Toaster />
+    </>
   );
 }
 
